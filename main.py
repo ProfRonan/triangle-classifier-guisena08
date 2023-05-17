4273dccca5afa35eefeca1aecdf3473c6c411d53
guisena08 <guisena@ufc.alu.br>
2023-05-17 12:35:44 -0300

    Erros corrigidos

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 a = int(input('digite um numero:'))
 b = int(input('digite um segundo número:'))
 c = int(input('digite um terceito numero:'))
-if (a + b) < c or (a + c) < b or b + c < a:
-    print('Não é um triângulo')
-elif a == b == c:
-    print('Equilatero')
-elif a == b or a == c or c == b:
-    print('Isóceles')  
-elif a != b or a != c or b != c:
-    print('Escaleno')     
+if (a + b) > c and (a + c) > b and b + c > a:
+    if a == b == c:
+        print('Equilátero')
+    elif a == b or a == c or c == b:
+       print('Isósceles')  
+    elif a != b or a != c or b != c:
+       print('Escaleno')  
+else:
+    print('Não é um triângulo')          
